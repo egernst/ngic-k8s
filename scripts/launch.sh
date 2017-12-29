cfe26346aeb48529b6df9feef370426b79a356de
Your Name <you@example.com>
2017-12-29 09:44:05 -0800

    removed hardcode ip

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -20,6 +20,7 @@ get_pcimac_addr () {
     cid="$cid-$ifname"
     eval "export $2=$(awk -F '"' '{print $4}' /sriov-cni/$cid)"
     eval "export $3=$(awk -F '"' '{print $8}' /sriov-cni/$cid)"
+    eval "export $4=$(awk -F '"' '{print $12}' /sriov-cni/$cid)"
 }
 
 
@@ -29,8 +30,8 @@ then
     cd /opt/ngic/bin
     echo "Configuring CP"
     # set the variables we provide
-    SGW_S11_IP=$(netstat -ie | grep -A1 s11-net | tail -1 | awk '{print $2}' | tr -d addr:)
-    CP_CPDP_IP=$(netstat -ie | grep -A1 cpdp-net | tail -1 | awk '{print $2}' | tr -d addr:)
+    SGW_S11_IP=$(netstat -ie | grep -A1 eth0 | tail -1 | awk '{print $2}' | tr -d addr:)
+    CP_CPDP_IP=$(netstat -ie | grep -A1 eth0 | tail -1 | awk '{print $2}' | tr -d addr:)
     consul kv put --http-addr=consul:8500 SGW_S11_IP $SGW_S11_IP
     consul kv put --http-addr=consul:8500 CP_CPDP_IP $CP_CPDP_IP
 
@@ -57,12 +58,8 @@ then
     # if we are using sriov, -w pciAddr
     if [ -d "/sriov-cni" ]; then
         echo "================== SR-IOV FOUND ============"
-        echo "TODO for now, manually put some dummy ips in consul"
-        SGW_S1U_IP="2.2.2.2"
-        SGW_SGI_IP="3.3.3.3"
-        
-        get_pcimac_addr s1u-net SGW_S1U_PCI S1U_MAC
-        get_pcimac_addr sgi-net SGW_SGI_PCI SGI_MAC
+        get_pcimac_addr s1u-net SGW_S1U_PCI S1U_MAC SGW_S1U_IP
+        get_pcimac_addr sgi-net SGW_SGI_PCI SGI_MAC SGW_SGI_IP
         DEVICES="-w $SGW_S1U_PCI -w $SGW_SGI_PCI"
 
 
@@ -77,7 +74,7 @@ then
         DEVICES="--no-pci --vdev eth_af_packet0,iface=s1u-net --vdev eth_af_packet1,iface=sgi-net"
     fi
 
-    DP_CPDP_IP=$(netstat -ie | grep -A1 cpdp-net | tail -1 | awk '{print $2}' | tr -d addr:)
+    DP_CPDP_IP=$(netstat -ie | grep -A1 eth0 | tail -1 | awk '{print $2}' | tr -d addr:)
 
     consul kv put --http-addr=consul:8500 SGW_S1U_IP $SGW_S1U_IP
     consul kv put --http-addr=consul:8500 SGW_SGI_IP $SGW_SGI_IP
@@ -104,16 +101,17 @@ then
     echo "Configuring Traffic Container"
     if [ -d "/sriov-cni" ]; then
         echo "================== SR-IOV FOUND ============"
-        RTR_SGI_IP=9.9.9.9
-        MME_S11_IP=10.10.10.10
-        ENB_S11_IP=11.11.11.11
+        get_pcimac_addr sgi-net RTR_SGI_PCI RTR_SGI_MAC RTR_SGI_IP
+        get_pcimac_addr sgi-net ENB_S1U_PCI ENB_S1U_MAC ENB_S1U_IP
+
     else #dev --vdev af_packt
         RTR_SGI_IP=$(netstat -ie | grep -A1 sgi-net | tail -1 | awk '{print $2}' | tr -d addr:)
-        MME_S11_IP=$(netstat -ie | grep -A1 s11-net | tail -1 | awk '{print $2}' | tr -d addr:)
 
         # Requires its own ENB address (but does not need to share)
-        ENB_S11_IP=$(netstat -ie | grep -A1 s1u-net | tail -1 | awk '{print $2}' | tr -d addr:)
+        ENB_S1U_IP=$(netstat -ie | grep -A1 s1u-net | tail -1 | awk '{print $2}' | tr -d addr:)
     fi
+ 
+    MME_S11_IP=$(netstat -ie | grep -A1 eth0 | tail -1 | awk '{print $2}' | tr -d addr:)
 
     # set the variables we provide
     consul kv put --http-addr=consul:8500 RTR_SGI_IP $RTR_SGI_IP
@@ -125,7 +123,7 @@ then
     get_from_consul
 
     # perform the traffic rewrites
-    ./rewrite_pcaps.py $ENB_S11_IP $SGW_S11_IP $SGW_S1U_IP $SGW_SGI_IP
+    ./rewrite_pcaps.py $ENB_S1U_IP $SGW_S11_IP $SGW_S1U_IP $SGW_SGI_IP
     sleep 3600
 
 else
