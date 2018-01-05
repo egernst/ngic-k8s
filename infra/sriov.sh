412d74a53c55cdc26cd83af2901fa0cde4e2eca0
Your Name <you@example.com>
2018-01-05 10:41:32 -0800

    fixing sriov script, should not set mac addr if not sriov

--- infra/sriov.sh
+++ infra/sriov.sh
@@ -6,6 +6,9 @@ echo 0 | sudo tee /sys/class/net/$1/device/sriov_numvfs
 echo 8 | sudo tee /sys/class/net/$1/device/sriov_numvfs
 sudo ip link set $1 up
 for i in {0..7}; do sudo ip link set $1 vf $i spoofchk off; done
-for i in {0..7}; do sudo ip link set $1 vf $i mac "c0:ff:ee:ab:b$i:0$2"; done
+if [ -d "/sriov-cni" ]; then
+    echo "Found SRIOV"
+    for i in {0..7}; do sudo ip link set $1 vf $i mac "c0:ff:ee:ab:b$i:0$2"; done
+fi
 for i in {0..7}; do sudo ip link set dev $1 vf $i state enable; done
 
