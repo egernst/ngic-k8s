621e6e73e28a3f3a359348746651eb45ec94be62
stack <stack@ilepc>
2018-01-11 12:25:11 -0800

    more sriov vfs

--- infra/sriov.sh
+++ infra/sriov.sh
@@ -3,8 +3,8 @@
 #Usage: ./sriov.sh ens785f0
 
 echo 0 | sudo tee /sys/class/net/$1/device/sriov_numvfs
-echo 8 | sudo tee /sys/class/net/$1/device/sriov_numvfs
+echo 32 | sudo tee /sys/class/net/$1/device/sriov_numvfs
 sudo ip link set $1 up
-for i in {0..7}; do sudo ip link set $1 vf $i spoofchk off; done
-for i in {0..7}; do sudo ip link set dev $1 vf $i state enable; done
+for i in {0..31}; do sudo ip link set $1 vf $i spoofchk off; done
+for i in {0..31}; do sudo ip link set dev $1 vf $i state enable; done
 
